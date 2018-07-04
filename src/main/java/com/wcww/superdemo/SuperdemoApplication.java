fed005623de9fd0db133d631d7e9393bbde8cfdc
huishanyi <huishanyicc@163.com>
2018-07-05 00:34:40 +0800

    初始化提交

---
+++ src/main/java/com/wcww/superdemo/SuperdemoApplication.java
@@ -0,0 +1,12 @@
+package com.wcww.superdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperdemoApplication.class, args);
+	}
+}
